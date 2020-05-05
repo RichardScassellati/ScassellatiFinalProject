956ba35e58640977a6294c9889b8c2dce8b81e7e
Richard Scassellati <rn.scassellati@setonhill.edu>
2020-05-04 20:26:39 -0400

    Initial Commit

---
+++ ScassellatiFinalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScassellatiFinalProject
+//
+//  Created by Richard Scassellati on 5/4/20.
+//  Copyright © 2020 Smecko Geck Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
